2b2cd123f93d6734f33308d8dbc58b7879ff1c5f
xXcoronaXx <acevedo.corona@gmail.com>
2015-03-03 17:52:13 +0100

    fix script

--- start.sh
+++ start.sh
@@ -4,10 +4,15 @@
 #
 # Written by Antonio Acevedo Bañez <acevedo.corona@gmail.com>
 # git clone https://github.com/xXcoronaXx/servidor-adhoc.git
-echo 'Arrancando servidor de nombrado...'
+echo '** Arrancando Bluetooth **'
+cd ..
+cd ecodroidlink/
+sudo ./edl_main &
+echo '** Arrancando servidor de nombrado... **'
 export PYRO_HMAC_KEY='the_same_string_for_server_and_client'
 python -m Pyro4.naming --host 192.168.1.115 &
-
-echo 'Arrancando servicio REST'
+cd 
+cd servidor-adhoc/
+echo '** Arrancando servicio REST ** '
 sudo python service.py &
-echo 'Servicio up!'
+echo '** Servicio up! **'
