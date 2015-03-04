23e35ea637351bc8237971511e2f550b09607f39
xXcoronaXx <acevedo.corona@gmail.com>
2015-03-04 22:28:33 +0100

    no poner como servicio

--- setservice.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Set server like a service
-#
-# Written by Antonio Acevedo Bañez <acevedo.corona@gmail.com>
-# git clone https://github.com/xXcoronaXx/servidor-adhoc.git
-sudo cp script/adhoc.conf /etc/init/
-echo 'You should then be able to run the script by typing sudo start adhoc, and kill it with sudo stop adhoc'
