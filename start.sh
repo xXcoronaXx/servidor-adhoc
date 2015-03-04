c24e57e29d765be3feda07a4512ee595e16f76eb
xXcoronaXx <acevedo.corona@gmail.com>
2015-03-04 17:49:36 +0100

    error script

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ echo '** Arrancando servidor de nombrado... **'
 export PYRO_HMAC_KEY='the_same_string_for_server_and_client'
 python -m Pyro4.naming --host 192.168.1.115 &
 sudo bluez-test-adapter name Pi
-cd 
+cd ..
 cd servidor-adhoc/
 echo '** Arrancando servicio REST ** '
 sudo python service.py &
