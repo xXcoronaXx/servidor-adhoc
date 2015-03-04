37e7dc8f86df95a66e02f5f59dd2305be18e6e6f
xXcoronaXx <acevedo.corona@gmail.com>
2015-03-04 19:02:56 +0100

    fix

--- start.sh
+++ start.sh
@@ -11,7 +11,6 @@ sudo ./edl_main &
 echo '** Arrancando servidor de nombrado... **'
 export PYRO_HMAC_KEY='the_same_string_for_server_and_client'
 python -m Pyro4.naming --host 192.168.1.115 &
-sudo bluez-test-adapter name Pi
 cd ..
 cd servidor-adhoc/
 echo '** Arrancando servicio REST ** '
