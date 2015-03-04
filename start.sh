74104ad30aac1748c723b2a804f989116e79176b
xXcoronaXx <acevedo.corona@gmail.com>
2015-03-04 21:36:20 +0100

    cambiando timing

--- start.sh
+++ start.sh
@@ -8,16 +8,16 @@ echo '** Arrancando Bluetooth **'
 cd ..
 cd ecodroidlink/
 sudo ./edl_main &
-sleep 5
+sleep 4
 echo '** Arrancando servidor de nombrado... **'
 export PYRO_HMAC_KEY='the_same_string_for_server_and_client'
 python -m Pyro4.naming --host 192.168.1.115 &
-sleep 1
+sleep 2
 cd ..
 cd servidor-adhoc/
 echo '** Arrancando servicio REST ** '
 sudo python service.py &
-sleep 3
+sleep 4
 echo '** Configurando Iptables... **'
 sudo sysctl net.ipv4.ip_forward=1
 sudo iptables -t nat -A PREROUTING -d 192.168.1.115 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.1:80
