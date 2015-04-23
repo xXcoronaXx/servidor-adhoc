01ea10cd32288fb0cc8b8799a8926431d40c3a50
xXcoronaXx <acevedo.corona@gmail.com>
2015-04-23 09:50:43 +0200

    pruebas

--- service.py
+++ service.py
@@ -15,10 +15,11 @@ class BroadCaster(object):
 		self.UDP_IP = '<broadcast>' # si no indicamos dirección lo envia a difusión
 		self.UDP_PORT = PUERTO_DIFUSION
 		self.sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		#self.sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sock1.bind(('',0))
 		self.sock1.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-		#self.sock2.bind(('192.168.0.1',0))
+		self.sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sock2.bind(('192.168.0.1',0))
+		self.sock2.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 		print "Caster creado"
 		print "UDP target IP: ", self.UDP_IP
 		print "UDP target port:", self.UDP_PORT
@@ -29,7 +30,7 @@ class BroadCaster(object):
 			while True:
 				print '* * Enviando * *'
 				self.sock1.sendto(self.MESSAGE, (self.UDP_IP, self.UDP_PORT))
-				#self.sock2.sendto(self.MESSAGE, (self.UDP_IP, self.UDP_PORT))
+				self.sock2.sendto(self.MESSAGE, (self.UDP_IP, self.UDP_PORT))
 				time.sleep( TIEMPO_ANUNCIOS )
 		# manejo de hilos
 		thread = threading.Thread(target=broadcast(self))
